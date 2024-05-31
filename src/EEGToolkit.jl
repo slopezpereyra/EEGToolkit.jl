1766b49f584166b594019cd6fa597a1fec0557ca
santi <santiagolopezpereyra@gmail.com>
2024-05-31 18:54:42 -0300

    Exported spindle algorithms

--- src/EEGToolkit.jl
+++ src/EEGToolkit.jl
@@ -8,7 +8,6 @@ export plot_eeg
 export plot_eeg_overlay
 export get_stage_indexes
 export get_stage 
-export sfilter!
 export artifact_reject
 
 export overlaps 
@@ -20,11 +19,15 @@ export next_power_of_two
 export zero_pad 
 export freq_band 
 
-export nrem 
+export nrem
+
+export sigma_index
+export relative_spindle_power
 
 
 include("eeg.jl")
 include("nrem.jl")
 include("psd.jl")
+include("spindles.jl")
 
 end
