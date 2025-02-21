4c20c820d0f2dcfb4aeaf6d531cfabb3ddd23f5d
santi <santiagolopezpereyra@gmail.com>
2025-02-21 12:51:35 -0300

    temporarily removed test suit2

--- src/nrem.jl
+++ src/nrem.jl
@@ -1,48 +1,48 @@
 
 # O(n)
 function insert_spaces_on_change(str)
-    # Initialize an empty string to build the modified version
-    modified_str = ""
+  # Initialize an empty string to build the modified version
+  modified_str = ""
 
-    # Iterate through the characters of the input string
-    for (index, char) in enumerate(str)
-        # Append the current character to the modified string
-        modified_str *= char
+  # Iterate through the characters of the input string
+  for (index, char) in enumerate(str)
+    # Append the current character to the modified string
+    modified_str *= char
 
-        # Check if we're at the last character or the next character is different
-        if index < length(str) && str[index] != str[index+1]
-            # Append a space to separate different character sequences
-            modified_str *= " "
-        end
+    # Check if we're at the last character or the next character is different
+    if index < length(str) && str[index] != str[index+1]
+      # Append a space to separate different character sequences
+      modified_str *= " "
     end
+  end
 
-    return modified_str
+  return modified_str
 end
 
 # O(n) + O(n) + O(n) = O(n)
 function stage_to_word(stages, m)
 
-    # Given a stages s₁, ..., sₙ, produces a sentence s.t. each word 
-    # in a sentence corresponds to segments of the staging with repeating 
-    # stages.
-    stages = replace(stages, "1" => "I", "?" => "I", "2" => "X", "3" => "X", "4" => "X")
-    α = insert_spaces_on_change(join(stages, ""))
+  # Given a stages s₁, ..., sₙ, produces a sentence s.t. each word 
+  # in a sentence corresponds to segments of the staging with repeating 
+  # stages.
+  stages = replace(stages, "1" => "I", "?" => "I", "2" => "X", "3" => "X", "4" => "X")
+  α = insert_spaces_on_change(join(stages, ""))
 
-    # A function to transform the words of α, effectively mapping α 
-    # onto a language more suited for regexes.
-    function transform(w)
-        if occursin('I', w) || occursin("X", w)
-            return w
-        end
-        # If previous condition was not met, we have either `5` or `6`.
-        if length(w) >= m
-            return (w)
-        end
-        repeat("I", length(w))
+  # A function to transform the words of α, effectively mapping α 
+  # onto a language more suited for regexes.
+  function transform(w)
+    if occursin('I', w) || occursin("X", w)
+      return w
     end
+    # If previous condition was not met, we have either `5` or `6`.
+    if length(w) >= m
+      return (w)
+    end
+    repeat("I", length(w))
+  end
 
-    trans = transform.(split(α))
-    return join(trans, "")
+  trans = transform.(split(α))
+  return join(trans, "")
 end
 
 """
@@ -66,59 +66,59 @@ wakefulness, and `?` denotes unscored/unstaged.
 """
 function nrem(staging::Vector, n::Integer=30, m::Integer=10)
 
-    if Set(unique(staging)) ⊈ Set(["1", "2", "3", "4", "5", "6", "?"])
-        e = "Invalid stage score: Sleep stages should be 1, 2, 3, 4, 5, 6, ?, 
-        where 5 marks REM, 6 wakefulness, and ? unknown/unscored."
-        throw( ArgumentError(e) )
-    end
+  if Set(unique(staging)) ⊈ Set(["1", "2", "3", "4", "5", "6", "?"])
+    e = "Invalid stage score: Sleep stages should be 1, 2, 3, 4, 5, 6, ?, 
+      where 5 marks REM, 6 wakefulness, and ? unknown/unscored."
+    throw( ArgumentError(e) )
+  end
 
-    α = stage_to_word(staging, m)
-    φ = Regex("(?:[X](I*)){$n,}(?:5|6{$m,}|\$)")
-    ζ = Regex("(?:[X](I*)){$n,}(?:[5](I*){$m,}|6{$m,}|\$)")
- 
-    # ------------------------------------
-    # Finding first and last NREM periods:
-    x = eachmatch(φ, α)
-    locs = []
-    for match in x
-        s = match.offset
-        e = match.offset + length(match.match) - 1
-        push!(locs, [s, e])
-    end
+  α = stage_to_word(staging, m)
+  φ = Regex("(?:[X](I*)){$n,}(?:5|6{$m,}|\$)")
+  ζ = Regex("(?:[X](I*)){$n,}(?:[5](I*){$m,}|6{$m,}|\$)")
 
-    if length(locs) == 0 
-        print("No NREM periods found. Returning nothing...")
-        return nothing
-    end
+  # ------------------------------------
+  # Finding first and last NREM periods:
+  x = eachmatch(φ, α)
+  locs = []
+  for match in x
+    s = match.offset
+    e = match.offset + length(match.match) - 1
+    push!(locs, [s, e])
+  end
 
-    # Keep only first and last matches.
-    locs = [locs[1], locs[length(locs)]]
-    s = (locs[1][2])
-    e = locs[length(locs)][1] - 1
+  if length(locs) == 0 
+    print("No NREM periods found. Returning nothing...")
+    return nothing
+  end
 
-    
-    # ------------------------------------
-    # Finding middle NREM periods.
+  # Keep only first and last matches.
+  locs = [locs[1], locs[length(locs)]]
+  s = (locs[1][2])
+  e = locs[length(locs)][1] - 1
 
-    # Split α so as to search for ζ in between the first and the last matches
-    middle = α[s:e]
-    matches = eachmatch(ζ, middle)
 
-    for match in matches
-        begins = s + match.offset - 1
-        loc = [begins, begins + length(match.match)]
-        insert!(locs, length(locs), loc)
-    end
+  # ------------------------------------
+  # Finding middle NREM periods.
 
-    # Collect all matches into vectors
-    locs = [collect(x[1]:x[2]) for x in locs]
+  # Split α so as to search for ζ in between the first and the last matches
+  middle = α[s:e]
+  matches = eachmatch(ζ, middle)
 
-    # Remove ending REM and Wakes as well as in-between REM, Wake, and Stage 1 epochs.
-    for nrem in locs
-        indx = findall(x -> staging[x] != "2" && staging[x] != "3" && staging[x] != "4", nrem)
-        deleteat!(nrem, indx)
-    end
+  for match in matches
+    begins = s + match.offset - 1
+    loc = [begins, begins + length(match.match)]
+    insert!(locs, length(locs), loc)
+  end
 
-    return locs
+  # Collect all matches into vectors
+  locs = [collect(x[1]:x[2]) for x in locs]
+
+  # Remove ending REM and Wakes as well as in-between REM, Wake, and Stage 1 epochs.
+  for nrem in locs
+    indx = findall(x -> staging[x] != "2" && staging[x] != "3" && staging[x] != "4", nrem)
+    deleteat!(nrem, indx)
+  end
+
+  return locs
 end
 
--- src/spindles.jl
+++ src/spindles.jl
@@ -22,31 +22,31 @@ Higher values are indicative of a higher spindle probability. The rejection
 threshold recommended in the original paper is ``\\lambda = 4.5``.
 """
 function sigma_index(x::Vector{<:AbstractFloat}, fs::Integer)
-    segs = segment(x, fs)
-    amps = map(x -> AmplitudeSpectrum(x, fs), segs)
+  segs = segment(x, fs)
+  amps = map(x -> AmplitudeSpectrum(x, fs), segs)
 
-    # Helper function `f`: Applys function `g` to the amplitude spectrum in a given 
-    # frequency band.
-    function f(amp::AmplitudeSpectrum, band_lower::Number, band_upper::Number, g::Function)
-        freq_band = findall(x -> x >= band_lower && x <= band_upper, amp.freq)
-        freq_band_power = amp.spectrum[freq_band]
-        g(freq_band_power)
-    end
-    
-    # Helper function. Computes the σ-index of an AmplitudeSpectrum.
-    function compute_index(amp::AmplitudeSpectrum)
-        spindle_max_power =  f(amp, 10.5, 16, maximum)
-        alpha_rejection_thresh = f(amp, 7.5, 10, maximum)
-        if alpha_rejection_thresh > spindle_max_power 
-            return 0
-        end
-        low_freq_mean_power = f(amp, 4, 10, y -> sum(y) / length(y))
-        high_freq_mean_power = f(amp, 20, 40, y -> sum(y) / length(y))
-        return 2 * spindle_max_power / ( low_freq_mean_power + high_freq_mean_power )
-    end
+  # Helper function `f`: Applys function `g` to the amplitude spectrum in a given 
+  # frequency band.
+  function f(amp::AmplitudeSpectrum, band_lower::Number, band_upper::Number, g::Function)
+    freq_band = findall(x -> x >= band_lower && x <= band_upper, amp.freq)
+    freq_band_power = amp.spectrum[freq_band]
+    g(freq_band_power)
+  end
 
-    Σ = map(compute_index, amps)
-    return(Σ)
+  # Helper function. Computes the σ-index of an AmplitudeSpectrum.
+  function compute_index(amp::AmplitudeSpectrum)
+    spindle_max_power =  f(amp, 10.5, 16, maximum)
+    alpha_rejection_thresh = f(amp, 7.5, 10, maximum)
+    if alpha_rejection_thresh > spindle_max_power 
+      return 0
+    end
+    low_freq_mean_power = f(amp, 4, 10, y -> sum(y) / length(y))
+    high_freq_mean_power = f(amp, 20, 40, y -> sum(y) / length(y))
+    return 2 * spindle_max_power / ( low_freq_mean_power + high_freq_mean_power )
+  end
+
+  Σ = map(compute_index, amps)
+  return(Σ)
 end
 
 """
@@ -69,19 +69,19 @@ probability (it should be clear that ``RSP`` is not a probability itself).
 The rejection threshold recommended in the original paper is ``\\lambda = 0.22``.
 """
 function relative_spindle_power(x::Vector{<:AbstractFloat}, fs::Integer)
-    segs = segment(x, fs)
-    amps = map(x -> AmplitudeSpectrum(x, fs, 512), segs)
-    
-    function f(amp::AmplitudeSpectrum, band_lower::Number, band_upper::Number, g::Function)
-        freq_band = findall(x -> x >= band_lower && x <= band_upper, amp.freq)
-        freq_band_power = amp.spectrum[freq_band]
-        g(freq_band_power)
-    end
+  segs = segment(x, fs)
+  amps = map(x -> AmplitudeSpectrum(x, fs, 512), segs)
 
-    spindle_band_powers = map(x -> f(x, 11, 15, sum), amps)
-    total_band_powers = map(x -> f(x, 0.5, 40, sum), amps)
+  function f(amp::AmplitudeSpectrum, band_lower::Number, band_upper::Number, g::Function)
+    freq_band = findall(x -> x >= band_lower && x <= band_upper, amp.freq)
+    freq_band_power = amp.spectrum[freq_band]
+    g(freq_band_power)
+  end
 
-    return spindle_band_powers ./ total_band_powers
+  spindle_band_powers = map(x -> f(x, 11, 15, sum), amps)
+  total_band_powers = map(x -> f(x, 0.5, 40, sum), amps)
+
+  return spindle_band_powers ./ total_band_powers
 end
 
 
